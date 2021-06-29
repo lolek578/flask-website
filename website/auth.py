8b59c95d920ad4df3d17b4ed7cddfb4594bc7048
lolek578 <patryk.labuz45@gmail.com>
2021-06-29 21:54:51 +0200

    Corrected sign-up method.

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request, flash
-
+import self as self
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import db
+from .models import User
+import flask_bcrypt
 
 auth = Blueprint('auth', __name__)
 
@@ -16,13 +19,13 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    if request.method=='POST':
+    if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        if '@' not in email:
             flash('Wrong email', category="error")
         elif len(firstName) < 2:
             flash('Wrong first name', category="error")
@@ -31,7 +34,11 @@ def sign_up():
         elif len(password1) < 10:
             flash('Wrong password', category="error")
         else:
+            new_user = User(email=email, firstName=firstName, password=flask_bcrypt.generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category="success")
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
 
