c077ff8d565b1eae1476ace6c4a545eefef5d44b
lolek578 <patryk.labuz45@gmail.com>
2021-06-29 21:04:25 +0200

    Created model of database

---
+++ website/models.py
@@ -0,0 +1,19 @@
+# models of database app
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(512))
+    firstName = db.Column(db.String(32))
+    notes = db.relationship('Note')
