c4fafd69836210dd15daa762c76c0370958884ca
lolek578 <patryk.labuz45@gmail.com>
2021-06-29 21:04:08 +0200

    Modified init

--- website/__init__.py
+++ website/__init__.py
@@ -3,17 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
-
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     load_dotenv()
+    # secrets
     SECRET_KEY = os.getenv('SECRET_KEY')
+    DB_NAME = os.getenv('DB_NAME')
+    DATABASE_URI = f'sqlite:///{DB_NAME}'
+
+    #config
     app.config['SECRET_KEY'] = SECRET_KEY
-    print(SECRET_KEY)
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
 
