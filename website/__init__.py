91f10a2bd337e2dd2ac31debb0b090d74fed7e4a
lolek578 <patryk.labuz45@gmail.com>
2021-06-29 20:22:38 +0200

    Secrets shouldn be stored in app code. Im modified my code. Now SECRET_KEY is stored in .env file, which is in .gitignore. Used os and load_dotenv.

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Lifeisputtingprozactothetest'
-
+    load_dotenv()
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    app.config['SECRET_KEY'] = SECRET_KEY
+    print(SECRET_KEY)
     from .views import views
     from .auth import auth
 
